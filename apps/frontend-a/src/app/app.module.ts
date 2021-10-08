29a5872f0c3f0eb01c30c2148457e17796a83f44
Lucas Simões <lucasousi@gmail.com>
2021-10-07 23:06:43 -0300

    Aplicação em Angular e React

---
+++ apps/frontend-a/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(
+      [
+        {
+          path: '',
+          pathMatch: 'full',
+          redirectTo: 'home-page',
+        },
+        {
+          path: 'home-page',
+          loadChildren: () =>
+            import('@primavera/frontend-a/home-page').then(
+              (module) => module.HomePageModule
+            ),
+        },
+      ],
+      { initialNavigation: 'enabledBlocking' }
+    ),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
